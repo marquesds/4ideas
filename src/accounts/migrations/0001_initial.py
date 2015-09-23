64812916d88fa0be200cb43594ff1b0c7912ee32
Lucas Marques <lucasmarquesds@gmail.com>
2015-09-23 00:32:56 -0300

    fixing heroku again...

--- src/accounts/migrations/0001_initial.py
+++ src/accounts/migrations/0001_initial.py
@@ -2,9 +2,9 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
-import django.contrib.auth.models
-import django.core.validators
 import re
+import django.core.validators
+import django.contrib.auth.models
 
 
 class Migration(migrations.Migration):
@@ -17,24 +17,24 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Player',
             fields=[
-                ('id', models.AutoField(serialize=False, verbose_name='ID', auto_created=True, primary_key=True)),
-                ('password', models.CharField(verbose_name='password', max_length=128)),
-                ('last_login', models.DateTimeField(null=True, blank=True, verbose_name='last login')),
-                ('is_superuser', models.BooleanField(help_text='Designates that this user has all permissions without explicitly assigning them.', default=False, verbose_name='superuser status')),
+                ('id', models.AutoField(serialize=False, primary_key=True, verbose_name='ID', auto_created=True)),
+                ('password', models.CharField(max_length=128, verbose_name='password')),
+                ('last_login', models.DateTimeField(verbose_name='last login', blank=True, null=True)),
+                ('is_superuser', models.BooleanField(default=False, verbose_name='superuser status', help_text='Designates that this user has all permissions without explicitly assigning them.')),
                 ('name', models.CharField(max_length=200)),
-                ('username', models.CharField(max_length=30, unique=True, verbose_name='Username', validators=[django.core.validators.RegexValidator(re.compile('^[\\w.+-]+$', 32), 'Username can only contain letters, digits or the following characters: ./+/-/_', 'invalid')])),
-                ('email', models.EmailField(unique=True, verbose_name='Email', max_length=150)),
-                ('bio', models.TextField(null=True, blank=True, verbose_name='Bio', max_length=5000)),
-                ('gender', models.CharField(max_length=6, default=('Male', 'Male'), choices=[('Male', 'Male'), ('Female', 'Female')])),
-                ('birth_date', models.DateField(null=True, blank=True, verbose_name='Birth Date')),
-                ('profile_image', models.CharField(default='/static/4ideas/img/default.png', max_length=300)),
-                ('cover_image', models.CharField(default='/static/4ideas/img/cover.jpeg', max_length=300)),
-                ('is_active', models.BooleanField(default=True, verbose_name='Is active?')),
-                ('is_staff', models.BooleanField(default=False, verbose_name='Is admin?')),
+                ('username', models.CharField(max_length=30, verbose_name='Username', unique=True, validators=[django.core.validators.RegexValidator(re.compile('^[\\w.+-]+$', 32), 'Username can only contain letters, digits or the following characters: ./+/-/_', 'invalid')])),
+                ('email', models.EmailField(max_length=150, verbose_name='Email', unique=True)),
+                ('bio', models.TextField(max_length=5000, verbose_name='Bio', blank=True, null=True)),
+                ('gender', models.CharField(null=True, max_length=6, blank=True, choices=[('Male', 'Male'), ('Female', 'Female')])),
+                ('birth_date', models.DateField(verbose_name='Birth Date', blank=True, null=True)),
+                ('profile_image', models.CharField(max_length=300, default='/static/4ideas/img/default.png')),
+                ('cover_image', models.CharField(max_length=300, default='/static/4ideas/img/cover.jpeg')),
+                ('is_active', models.BooleanField(verbose_name='Is active?', default=True)),
+                ('is_staff', models.BooleanField(verbose_name='Is admin?', default=False)),
                 ('date_joined', models.DateTimeField(auto_now_add=True)),
                 ('updated_at', models.DateTimeField(auto_now=True)),
-                ('groups', models.ManyToManyField(related_query_name='user', blank=True, verbose_name='groups', related_name='user_set', to='auth.Group', help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.')),
-                ('user_permissions', models.ManyToManyField(related_query_name='user', blank=True, verbose_name='user permissions', related_name='user_set', to='auth.Permission', help_text='Specific permissions for this user.')),
+                ('groups', models.ManyToManyField(related_name='user_set', to='auth.Group', blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', verbose_name='groups', related_query_name='user')),
+                ('user_permissions', models.ManyToManyField(related_name='user_set', to='auth.Permission', blank=True, help_text='Specific permissions for this user.', verbose_name='user permissions', related_query_name='user')),
             ],
             options={
                 'verbose_name': 'Player',
